0cc947dfbd92aae9cb4c09824f65ba33e6abfc06
Supchik18 <144320382+Supchik18@users.noreply.github.com>
2023-09-28 11:24:47 +0300

    Update Калькулятор.py

--- Калькулятор.py
+++ Калькулятор.py
@@ -1,6 +1,7 @@
 import math
 
 while True:
+    
     print("Выберите операцию:")
     print("1. Сложение")
     print("2. Вычитание")
@@ -14,61 +15,98 @@ while True:
     print("10. Синус")
     print("11. Косинус")
     print("12. Тангенс")
-
-    choice = int(input("Введите номер операции: "))
-
+    while True:
+        try:
+            choice = int(input("Введите номер операции: "))
+            break
+        except ValueError:
+            print ("Нужно ввести число, а не буквы")
     if choice == 1:
-        num1 = float(input("Введите первое число: "))
-        num2 = float(input("Введите второе число: "))
-        result = num1 + num2
-        print("Результат:", result)
+        try:
+            num1 = float(input("Введите первое число: "))
+            num2 = float(input("Введите второе число: "))
+            result = num1 + num2
+            print("Результат:", result)
+        except ValueError:
+            print("Нужно ввести число,а не буквы")
     elif choice == 2:
-        num1 = float(input("Введите первое число: "))
-        num2 = float(input("Введите второе число: "))
-        result = num1 - num2
-        print("Результат:", result)
+        try:
+            num1 = float(input("Введите первое число: "))
+            num2 = float(input("Введите второе число: "))
+            result = num1 - num2
+            print("Результат:", result)
+        except ValueError:
+            print("Нужно ввести число,а не буквы")
     elif choice == 3:
-        num1 = float(input("Введите первое число: "))
-        num2 = float(input("Введите второе число: "))
-        result = num1 * num2
-        print("Результат:", result)
+        try:
+            num1 = float(input("Введите первое число: "))
+            num2 = float(input("Введите второе число: "))
+            result = num1 * num2
+            print("Результат:", result)
+        except ValueError:
+            print("Нужно ввести число,а не буквы")
     elif choice == 4:
-        num1 = float(input("Введите первое число: "))
-        num2 = float(input("Введите второе число: "))
-        result = num1 / num2
-        print("Результат:", result)
+        try:
+            num1 = float(input("Введите первое число: "))
+            num2 = float(input("Введите второе число: "))
+            result = num1 / num2
+            print("Результат:", result)
+        except ValueError:
+            print("Нужно ввести число,а не буквы")
     elif choice == 5:
-        num1 = float(input("Введите число: "))
-        power = int(input("Введите степень: "))
-        result = num1 ** power
-        print("Результат:", result)
+        try:
+            num1 = float(input("Введите число: "))
+            power = int(input("Введите степень: "))
+            result = num1 ** power
+            print("Результат:", result)
+        except ValueError:
+            print("Нужно ввести число,а не буквы")
     elif choice == 6:
-        num = float(input("Введите число: "))
-        result = math.sqrt(num)
-        print("Результат:", result)
+        try:
+            choice = int(input("Введите число "))
+            print("Результат ",choice**0.5)
+        except ValueError:
+            print("Нужно ввести число,а не буквы")
     elif choice == 7:
-        num = float(input("Введите число: "))
-        result = num * 0.01
-        print("Результат:", result)
+        try:
+            num = float(input("Введите число: "))
+            result = num * 0.01
+            print("Результат:", result)
+        except ValueError:
+            print("Нужно ввести число,а не буквы")
     elif choice == 8:
-        num = int(input("Введите число: "))
-        result = 1
-        for i in range(1, num + 1):
-            result *= i
-        print("Результат:", result)
+        try:
+            num = int(input("Введите число: "))
+            result = 1
+            for i in range(1, num + 1):
+                result *= i
+            print("Результат:", result)
+        except ValueError:
+            print("Нужно ввести число,а не буквы")
     elif choice == 9:
-        break
+        try:
+            break
+        except ValueError:
+            print("Нужно ввести число,а не буквы")
     elif choice == 10:
-        num = float(input("Введите число: "))
-        result = math.sin(num)
-        print("Результат:", result)
+        try:
+            num = float(input("Введите число: "))
+            result = math.sin(num)
+            print("Результат:", result)
+        except ValueError:
+            print("Нужно ввести число,а не буквы")
     elif choice == 11:
-        num = float(input("Введите число: "))
-        result = math.cos(num)
-        print("Результат:", result)
+        try:
+            num = float(input("Введите число: "))
+            result = math.cos(num)
+            print("Результат:", result)
+        except ValueError:
+            print("Нужно ввести число,а не буквы")
     elif choice == 12:
-        num = float(input("Введите число: "))
-        result = math.tan(num)
-        print("Результат:", result)
-    else:
-        print("Некорректный номер операции. Попробуйте снова.")
+        try:
+            num = float(input("Введите число: "))
+            result = math.tan(num)
+            print("Результат:", result)
+        except ValueError:
+            print("Нужно ввести число,а не буквы")
+
